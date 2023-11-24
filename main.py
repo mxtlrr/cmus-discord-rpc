d59c2ccfb07a5b340d0057925c4fbba52841eb3d
mxtlrr <ruson0914@gmail.com>
2023-11-24 13:32:41 -0600

    barebones script done

---
+++ main.py
@@ -0,0 +1,18 @@
+from pypresence import Presence
+import time, subprocess
+
+
+client_id = 1177691132185018418
+RPC = Presence(client_id)
+RPC.connect()
+
+while True:
+  # get values
+  song = subprocess.check_output("./get.sh song", shell=True).decode("utf-8")
+  author = subprocess.check_output("./get.sh author", shell=True).decode("utf-8")
+  duration = subprocess.check_output("./get.sh duration", shell=True).decode("utf-8")
+  
+  ## update the rich presence
+  RPC.update(details=f"{song} by {author}",
+            state=f"{duration}")
+  time.sleep(1) # wait one second
